d412d73d44ba60e2aab81bda2eed3ab1f82d468d
Ilia Benkovitch <ibenkovitch@apple.com>
2019-12-14 12:52:38 +0200

    Added function for mirroring image

--- code/data_manipulation_functions.py
+++ code/data_manipulation_functions.py
@@ -1,11 +1,24 @@
 from PIL import Image
+from PIL import ImageOps
 # from resizeimage import resizeimage
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import ntpath
+import os
 
 # img = Image.open('image.png').convert('LA')
 # img.save('greyscale.png')
+
+def get_file_base_name(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+def get_file_base_name_without_extension(path):
+    name_with_extension = get_file_base_name(path)
+    name = name_with_extension.split('.')[0]
+
+    return name
 
 def rgb2grey(img):
     '''
@@ -28,19 +41,39 @@ def resize_image(img, size):
 
     return img_resized
 
-def prepare_image_to_fit_learning_and_save(img, size, dst_path):
+def flip_image(img):
+    img_flipped = ImageOps.mirror(img)
+
+    return img_flipped
+
+def prepare_image_to_fit_learning_and_save(img_path, size, dst_path):
+    if os.path.isdir(dst_path) is False:
+        assert False, "dst_path should be a directory"
+
+    if os.path.isdir(img_path) is True:
+        assert False, "img_path should not be a directory"
+
+    img = Image.open(img_path)
+
+    file_name_without_extension = get_file_base_name_without_extension(img_path)
+
+    grey_resized_file_name = file_name_without_extension + '_GR.png'
+    grey_resized_flipped_file_name = file_name_without_extension + '_GRF.png'
+
+    grey_resized_file_path = os.path.join(dst_path, grey_resized_file_name)
+
+    grey_resized_flipped_file_path = os.path.join(dst_path, grey_resized_flipped_file_name)
+
     img_grey_resized = resize_image(rgb2grey(img), size)
-    img_grey_resized.save(dst_path)
+    img_grey_resized.save(grey_resized_file_path)
+
+    img_grey_resized_flipped = flip_image(img_grey_resized)
+    img_grey_resized_flipped.save(grey_resized_flipped_file_path)
 
 if __name__ == '__main__':
-    img = Image.open('/Users/iliabenkovitch/Documents/Computer_Vision/git/tmp/test.jpg')
+
+    img_path = '/Users/iliabenkovitch/Documents/Computer_Vision/git/tmp/test.jpg'
 
     size = np.array([800, 600])
-    dst_path = '/Users/iliabenkovitch/Documents/Computer_Vision/git/tmp/test_gery_rescaled.png'
-    prepare_image_to_fit_learning_and_save(img, size, dst_path)
-
-    # img_grey_resized = resize_image(rgb2grey(img), size)
-    # plt.figure()
-    # plt.imshow(img_grey_resized)
-
-    # plt.show()
+    dst_path = '/Users/iliabenkovitch/Documents/Computer_Vision/git/tmp/'
+    prepare_image_to_fit_learning_and_save(img_path, size, dst_path)
